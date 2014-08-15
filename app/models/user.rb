46f59883d7b9fea8452e391d3c20a1e0ad4a8c14
prthmsk <prathameshmsk@gmail.com>
2014-08-15 15:01:03 +0530

    Added flash class helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ActiveRecord::Base
 	
   	has_many :statuses
 
+  	validates :first_name, presence: true
+  	validates :last_name, presence: true
+  	validates :profile_name, presence: true,
+  						 	uniqueness: { case_sensitive: false }, 
+  						 	format: {
+  						 		with: /\A[a-zA-Z0-9]+\z/,
+  						 		message: 'Must be formatted correctly.'
+  						 	}
+
 
 	def full_name
 		first_name + " " + last_name
